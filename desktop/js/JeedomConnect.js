4619c59cb83e77e91533191fbedf297772393ed4
ToMtOm <tomtom.infos@gmail.com>
2021-04-07 19:33:41 +0200

    FIX unit typo

--- desktop/js/JeedomConnect.js
+++ desktop/js/JeedomConnect.js
@@ -1371,13 +1371,13 @@ function downCmdOption(elm, optionsJson) {
 function refreshMoreInfos() {
   let div = '';
   moreInfos.forEach(item => {
-    var unite = item.unite || '';
+    var unit = item.unit || '';
     div += `<div class='input-group' style="border-width:1px; border-style:dotted;" id="moreInfo-${item.id}">
           <input style="width:260px;" class='input-sm form-control roundedLeft' id="${item.id}-input" value='${item.human}' disabled>
           <label style="position:absolute; margin-left:5px; width: 40px;"> Nom : </label>
           <input style="width:80px;position:absolute; margin-left:45px;" id="${item.id}-name-input" value='${item.name}'>
           <label style="position:absolute; margin-left:130px; width: 42px;"> Unité : </label>
-          <input style="width:80px;position:absolute; margin-left:175px;" id="${item.id}-unit-input" value='${unite}'>
+          <input style="width:80px;position:absolute; margin-left:175px;" id="${item.id}-unit-input" value='${unit}'>
           <i class="mdi mdi-minus-circle" style="color:rgb(185, 58, 62);font-size:24px;position:absolute; margin-left:260px;" aria-hidden="true" onclick="deleteMoreInfo('${item.id}');"></i>
           </div>`;
   });
@@ -1389,7 +1389,7 @@ function refreshMoreInfos() {
 function addMoreCmd() {
   jeedom.cmd.getSelectModal({cmd: {type: 'info' } }, function(result) {
     getCmdDetail({id:result.cmd.id, human:result.human}, function(result, _param){
-      moreInfos.push({ type: 'cmd', id: result.id, human: _param.human,  name: result.name, unite: result.unite});
+      moreInfos.push({ type: 'cmd', id: result.id, human: _param.human,  name: result.name, unit: result.unite});
       saveImgOption();
       refreshMoreInfos();
     })
@@ -1916,7 +1916,7 @@ function saveWidget() {
       result.moreInfos = [];
       moreInfos.forEach(info => {
         info.name = $("#"+info.id+"-name-input").val();
-        info.unite = $("#"+info.id+"-unit-input").val();
+        info.unit = $("#"+info.id+"-unit-input").val();
         result.moreInfos.push(info);
       });
     }
