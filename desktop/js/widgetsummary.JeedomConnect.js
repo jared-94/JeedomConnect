9a6c8ed97bc6fa90b9891732fbb4e881cf2ee193
ToMtOm <tomtom.infos@gmail.com>
2021-05-21 16:26:42 +0200

    ADD export button for summaryWidget

--- desktop/js/widgetsummary.JeedomConnect.js
+++ desktop/js/widgetsummary.JeedomConnect.js
@@ -224,3 +224,68 @@ $(document).ready(function(){
     displayWidgetSummaryData();
     
 });
+
+function exportTableToCSV($table, filename) {
+    
+    var $rows = $table.find('tr:has(td),tr:has(th)'),
+
+        // Temporary delimiter characters unlikely to be typed by keyboard
+        // This is to avoid accidentally splitting the actual contents
+        tmpColDelim = String.fromCharCode(11), // vertical tab character
+        tmpRowDelim = String.fromCharCode(0), // null character
+
+        // actual delimiter characters for CSV format
+        colDelim = '";"',
+        rowDelim = '"\r\n"',
+
+        // Grab text from table into CSV formatted string
+        csv = '"' + $rows.map(function (i, row) {
+            var $row = jQuery(row), $cols = $row.find('td,th');
+
+            return $cols.map(function (j, col) {
+                var $col = jQuery(col);
+                
+                if ( $col.find(">:first-child").is("input:checkbox") ){
+                    text = $col.find(":checkbox").is(":checked");
+                }
+                else if ( $col.find(">:first-child").is("input") ){
+                    text = $col.find("input").val();
+                }
+                else if ( $col.find(">:first-child").is("select") ){
+                    text = $col.find("option:selected").text();
+                }
+                else{
+                    text = $col.text();
+                }
+
+                if ( jQuery.type( text ) === "boolean" ){
+                    return text;
+                }
+                return text.replace(/"/g, '""'); // escape double quotes
+
+            }).get().join(tmpColDelim);
+
+        }).get().join(tmpRowDelim)
+            .split(tmpRowDelim).join(rowDelim)
+            .split(tmpColDelim).join(colDelim) + '"',
+
+        
+
+        // Data URI
+        csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
+        
+        // console.log(csv);
+        
+      if (window.navigator.msSaveBlob) { // IE 10+
+        //alert('IE' + csv);
+        window.navigator.msSaveOrOpenBlob(new Blob([csv], {type: "text/plain;charset=utf-8;"}), "csvname.csv")
+      } 
+      else {
+        jQuery(this).attr({ 'download': filename, 'href': csvData, 'target': '_blank' }); 
+      }
+}
+
+jQuery("#bt_exportJcWidgetSummary").on('click', function (event) { 
+    filename = 'export_JcWidgetSummary_' + new Date().toLocaleDateString() + '.csv';
+    exportTableToCSV.apply(this, [jQuery('#table_JcWidgetSummary'), filename]);
+});
