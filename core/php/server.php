d70f0a8764234452596364abe3ba2f6b6a99c4d9
ToMtOm <tomtom.infos@gmail.com>
2021-06-30 22:08:06 +0200

    FIX error msg on daemon port already used

--- core/php/server.php
+++ core/php/server.php
@@ -8,28 +8,28 @@ use JeedomConnectLogic\ConnectLogic;
 require_once dirname(__FILE__) . '/../../3rdparty/vendor/autoload.php';
 require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
 
-
-$port = intval(config::byKey('port', 'JeedomConnect', 8090));
-$versionPath = dirname(__FILE__) . '/../../plugin_info/version.json';
-$versionJson = json_decode(file_get_contents($versionPath));
-
-$connectLogic = new ConnectLogic($versionJson);
-
-
-// Create socket server
-$server = IoServer::factory(new HttpServer(new WsServer($connectLogic)), $port);
-
-// Add the periodic processing
-$server->loop->addPeriodicTimer(
-    1,
-    function () use ($connectLogic) {
-        $connectLogic->process();
-    }
-);
-
-// Run server
-log::add('JeedomConnect', 'info', "Listenning on port $port");
 try {
+    $port = intval(config::byKey('port', 'JeedomConnect', 8090));
+    $versionPath = dirname(__FILE__) . '/../../plugin_info/version.json';
+    $versionJson = json_decode(file_get_contents($versionPath));
+
+    $connectLogic = new ConnectLogic($versionJson);
+
+
+    // Create socket server
+    $server = IoServer::factory(new HttpServer(new WsServer($connectLogic)), $port);
+
+    // Add the periodic processing
+    $server->loop->addPeriodicTimer(
+        1,
+        function () use ($connectLogic) {
+            $connectLogic->process();
+        }
+    );
+
+    // Run server
+    log::add('JeedomConnect', 'info', "Listenning on port $port");
+
     $server->run();
 } catch (\Exception $e) {
     log::add('JeedomConnect', 'error', 'Daemon crash with following error: ' . $e->getMessage());
