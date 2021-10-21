029944c6dde7c323afca7cc775e35b4143978e04
ToMtOm <tomtom.infos@gmail.com>
2021-10-21 08:38:40 +0200

    FIX error on configData undefined

--- desktop/js/JeedomConnect.js
+++ desktop/js/JeedomConnect.js
@@ -2548,14 +2548,14 @@ function getWidgetPath(id) {
   var widget = allWidgetsDetail.find(w => w.id == id);
   var name = (' ' + widget.name).slice(1);
 
-  if (widget.parentId === undefined | widget.parentId == null) {
+  if (widget.parentId === undefined || widget.parentId == null || typeof configData === 'undefined') {
     return name;
   }
   var id = (' ' + widget.parentId.toString()).slice(1);
   parent = configData.payload.groups.find(i => i.id == id);
   if (parent) {
     name = parent.name + " / " + name;
-    if (parent.parentId === undefined | parent.parentId == null) {
+    if (parent.parentId === undefined || parent.parentId == null) {
       return name;
     }
     id = (' ' + parent.parentId.toString()).slice(1);
@@ -2563,7 +2563,7 @@ function getWidgetPath(id) {
   parent2 = configData.payload.sections.find(i => i.id == id);
   if (parent2) {
     name = parent2.name + " / " + name;
-    if (parent2.parentId === undefined | parent2.parentId == null) {
+    if (parent2.parentId === undefined || parent2.parentId == null) {
       return name;
     }
     id = (' ' + parent2.parentId.toString()).slice(1);
