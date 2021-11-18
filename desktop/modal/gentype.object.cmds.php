106e6c06b7e8fd9c38e48751b470d92281c9eee2
ToMtOm <tomtom.infos@gmail.com>
2021-11-18 09:18:37 +0100

    FIX typo && display eq if cmd exist

--- desktop/modal/gentype.object.cmds.php
+++ desktop/modal/gentype.object.cmds.php
@@ -48,78 +48,81 @@ if (is_object($object)) {
 echo '<div class="panel-group" id="accordionObjects">';
 
 foreach ($eqLogics as $eqLogic) {
-	echo '<div class="panel panel-default">';
-
-	echo '<div class="panel-heading">';
-	echo '<h3 class="panel-title">';
-	echo '<a class="accordion-toggle" data-toggle="collapse" aria-expanded="false" data-parent="" href="#eqLogic_' . $eqLogic->getId() . '"><span class="eqLogicAttr hidden" data-l1key="id">' . $eqLogic->getId() . '</span>' . $eqLogic->getName() . '</a>';
-	echo '</h3>';
-	echo '</div>';
-
-	echo '<div id="eqLogic_' . $eqLogic->getId() . '" class="panel-collapse collapse">';
-	echo '<div class="panel-body">';
 	$cmds = cmd::byEqLogicId($eqLogic->getId());
-	echo '<table id=' . $eqLogic->getId() . ' class="table table-bordered table-condensed tableCmd">';
-	echo '<tr>';
-	echo '<th>{{Nom}}</th>';
-	echo '<th>{{Sous-type}}</th>';
-	echo '<th>{{Type Générique}}</th>';
-	echo '</tr>';
-	foreach ($cmds as $cmd) {
-		echo '<tr class="cmdLine">';
-		echo '<td class="cmdName">' . $cmd->getName() . '</td>';
-		echo "<td class='cmdType'>{$cmd->getType()}/{$cmd->getSubType()}</td>";
-		echo '<td>';
-		echo '<span class="cmdAttr" data-l1key="id" style="display:none;">' . $cmd->getId() . '</span>';
-?>
-		<select class="cmdAttr form-control" data-l1key="generic_type" data-cmd_id="<?php echo $cmd->getId(); ?>">
-			<option value="">{{Aucun}}</option>
-			<?php
-			$groups = array();
-			foreach (jeedom::getConfiguration('cmd::generic_type') as $key => $info) {
-				if ($cmd->getType() == 'info' && $info['type'] == 'Action') {
-					continue;
-				} elseif ($cmd->getType() == 'action' && $info['type'] == 'Info') {
-					continue;
-				} elseif (isset($info['ignore']) && $info['ignore'] == true) {
-					continue;
-				}
-				$info['key'] = $key;
-				if (!isset($groups[$info['family']])) {
-					$groups[$info['family']][0] = $info;
-				} else {
-					array_push($groups[$info['family']], $info);
-				}
-			}
-			ksort($groups);
-			foreach ($groups as $group) {
-				usort($group, function ($a, $b) {
-					return strcmp($a['name'], $b['name']);
-				});
-				foreach ($group as $key => $info) {
-					if ($key == 0) {
-						echo '<optgroup label="{{' . $info['family'] . '}}">';
-					}
-					if ($info['key'] == 'DONT') continue;
-					if ($info['key'] == $cmd->getGeneric_type()) {
-						echo '<option value="' . $info['key'] . '" selected>' .  $info['name'] . '</option>';
-					} else {
-						echo '<option value="' . $info['key'] . '">'  . $info['name'] . '</option>';
-					}
-				}
-				echo '</optgroup>';
-			}
-			?>
-		</select>
-<?php
-		echo '</td>';
+	if (count($cmds) > 0) {
+		echo '<div class="panel panel-default">';
+
+		echo '<div class="panel-heading">';
+		echo '<h3 class="panel-title">';
+		echo '<a class="accordion-toggle" data-toggle="collapse" aria-expanded="false" data-parent="" href="#eqLogic_' . $eqLogic->getId() . '"><span class="eqLogicAttr hidden" data-l1key="id">' . $eqLogic->getId() . '</span>' . $eqLogic->getName() . '</a>';
+		echo '</h3>';
+		echo '</div>';
+
+		echo '<div id="eqLogic_' . $eqLogic->getId() . '" class="panel-collapse collapse">';
+		echo '<div class="panel-body">';
+		echo '<table id=' . $eqLogic->getId() . ' class="table table-bordered table-condensed tableCmd">';
+		echo '<tr>';
+		echo '<th>{{Nom}}</th>';
+		echo '<th>{{Sous-type}}</th>';
+		echo '<th>{{Type Générique}}</th>';
 		echo '</tr>';
+		foreach ($cmds as $cmd) {
+			echo '<tr class="cmdLine">';
+			echo '<td class="cmdName">' . $cmd->getName() . '</td>';
+			echo "<td class='cmdType'>{$cmd->getType()}/{$cmd->getSubType()}</td>";
+			echo '<td>';
+			echo '<span class="cmdAttr" data-l1key="id" style="display:none;">' . $cmd->getId() . '</span>';
+?>
+			<select class="cmdAttr form-control" data-l1key="generic_type" data-cmd_id="<?php echo $cmd->getId(); ?>">
+				<option value="">{{Aucun}}</option>
+				<?php
+				$groups = array();
+				foreach (jeedom::getConfiguration('cmd::generic_type') as $key => $info) {
+					if ($cmd->getType() == 'info' && $info['type'] == 'Action') {
+						continue;
+					} elseif ($cmd->getType() == 'action' && $info['type'] == 'Info') {
+						continue;
+					} elseif (isset($info['ignore']) && $info['ignore'] == true) {
+						continue;
+					}
+					$info['key'] = $key;
+					if (!isset($groups[$info['family']])) {
+						$groups[$info['family']][0] = $info;
+					} else {
+						array_push($groups[$info['family']], $info);
+					}
+				}
+				ksort($groups);
+				foreach ($groups as $group) {
+					usort($group, function ($a, $b) {
+						return strcmp($a['name'], $b['name']);
+					});
+					foreach ($group as $key => $info) {
+						if ($key == 0) {
+							echo '<optgroup label="{{' . $info['family'] . '}}">';
+						}
+						if ($info['key'] == 'DONT') continue;
+						if ($info['key'] == $cmd->getGeneric_type()) {
+							echo '<option value="' . $info['key'] . '" selected>' .  $info['name'] . '</option>';
+						} else {
+							echo '<option value="' . $info['key'] . '">'  . $info['name'] . '</option>';
+						}
+					}
+					echo '</optgroup>';
+				}
+				?>
+			</select>
+<?php
+			echo '</td>';
+			echo '</tr>';
+		}
+		echo '</table>';
+		echo '</div>';
+		echo '</div>';
+		echo '</div>';
 	}
-	echo '</table>';
-	echo '</div>';
-	echo '</div>';
-	echo '</div>';
 }
+echo '</div>';
 ?>
 
 <?php
