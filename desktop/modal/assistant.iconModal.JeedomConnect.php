23fa57709ab416a97c9aa2354e73eb776502dad1
ToMtOm <tomtom.infos@gmail.com>
2021-12-05 11:22:46 +0100

    fix

--- desktop/modal/assistant.iconModal.JeedomConnect.php
+++ desktop/modal/assistant.iconModal.JeedomConnect.php
@@ -214,7 +214,7 @@ function get_all_files($dir, $includeSubDir = false) {
           $div .= '<div class="cursor iconSel">';
           $div .= '<img source="user" name="' . $img['name'] . '" class="img-responsive" src="' . $img['path'] . '"/></div>';
           $div .= '<div class="iconDesc">' . $img['name'];
-          $div .= '&nbsp;&nbsp;<i class="fas fa-minus-circle bt_removeImg" style="color: red;padding-left: 5px;" data-realfilepath="' . $img['path'] . '"></i>';
+          $div .= '<i class="fas fa-minus-circle bt_removeImg" style="color: red;padding-left: 5px;" data-realfilepath="' . $img['path'] . '"></i>';
           $div .= '</div>';
           $div .= '</div>';
         }
