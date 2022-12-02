e58ec567f97d84605ddc24362f1b4d8bddd9f532
ToMtOm <tomtom.infos@gmail.com>
2022-12-02 18:02:51 +0100

    FIX img for component

--- core/class/apiHelper.class.php
+++ core/class/apiHelper.class.php
@@ -1704,10 +1704,20 @@ class apiHelper {
 
     foreach ($widgets as $i => $widget) {
       $imgPath = '';
-      foreach ($widgetsConfigJonFile['widgets'] as $config) {
-        if ($config['type'] == $widget['type']) {
-          $imgPath = 'plugins/JeedomConnect/data/img/' . $config['img'];
-          break;
+      if ($widget['type'] == 'component') {
+        foreach ($widgetsConfigJonFile['components'] as $config) {
+          if ($config['type'] == $widget['component']) {
+            $imgPath = 'plugins/JeedomConnect/data/img/' . $config['img'];
+            break;
+          }
+        }
+      } else {
+
+        foreach ($widgetsConfigJonFile['widgets'] as $config) {
+          if ($config['type'] == $widget['type']) {
+            $imgPath = 'plugins/JeedomConnect/data/img/' . $config['img'];
+            break;
+          }
         }
       }
       $newConfWidget['imgPath'] = $imgPath;
